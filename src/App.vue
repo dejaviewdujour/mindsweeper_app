21eebf82438573c073ee0d0a45377c8eebb7b0c1
zero4994 <cavc94@gmail.com>
2019-03-20 21:25:46 +0900

    Adds basic layout for the landing page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navbar/>
+    <home/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    navbar: Navbar,
+    home: Home
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <section>
+      <v-jumbotron
+        :aspect-ratio="16/9"
+        dark
+        :src="images.background"
+        class="overlay"
+        max-heigth="5rem"
+      >
+        <v-container fill-height>
+          <v-layout align-center>
+            <v-flex text-xs-center>
+              <h1 class="mt-5">Do you know what facebook has on you?</h1>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-jumbotron>
+    </section>
+    <section>
+      <h1>Before you start</h1>
+      <hr>
+      <p>Before we can use the app, you will need to personally download your Facebook data. This can only be done by you. The files you will download contain valuable information (including some you might not expect). To secure your privacy, please take the following instructions:</p>
+    </section>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data: () => ({
+    images: { background: require("@/assets/textlogo.png") }
+  })
+};
+</script>
+
+
+<style>
+.overlay {
+  background: rgba(0, 0, 0, 0.4);
+}
+.mt-5 {
+  padding-top: 15rem;
+}
+</style>
