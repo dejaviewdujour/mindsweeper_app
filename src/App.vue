7a7067bc1a4e0d3dff739cee1189178a695e5f16
zero4994 <cavc94@gmail.com>
2019-03-21 19:55:51 +0900

    Adds download component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <navbar v-on:changeView="changeView"/>
     <home v-if="currentView === 'home'" v-on:changeView="changeView"/>
     <about v-if="currentView === 'about'" v-on:changeView="changeView"/>
+    <download v-if="currentView === 'download'" v-on:changeView="changeView"/>
     <customFooter/>
   </div>
 </template>
@@ -11,6 +12,7 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
+import Download from "./components/Download";
 import Footer from "./components/Footer";
 
 export default {
@@ -19,6 +21,7 @@ export default {
     navbar: Navbar,
     home: Home,
     about: About,
+    download: Download,
     customFooter: Footer
   },
   data: () => ({
