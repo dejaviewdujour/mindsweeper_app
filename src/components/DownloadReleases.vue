490fe5ba0256ab6d1311c3029067935ee0ef13eb
zero4994 <cavc94@gmail.com>
2019-03-25 16:52:08 +0900

    Adds first implementation of loading spinner

--- src/components/DownloadReleases.vue
+++ src/components/DownloadReleases.vue
@@ -29,7 +29,11 @@
     </v-container>
     <v-container grid-list-md>
       <v-layout row wrap>
-        <v-flex sm6 v-for="(item, index) in releases" :key="index">
+        <div class="half-circle-spinner spinner" v-if="isLoading">
+          <div class="circle circle-1"></div>
+          <div class="circle circle-2"></div>
+        </div>
+        <v-flex sm6 v-for="(item, index) in releases" :key="index" v-else>
           <v-card>
             <v-card-title>
               <h2>{{item.Key}}</h2>
@@ -43,11 +47,17 @@
                     outline
                     block
                     color="indigo"
-                    @click="requestLink(item.Key)"
+                    @click="requestLink(item)"
                   >Request Link</v-btn>
                 </v-flex>
                 <v-flex sm6 v-if="item.url">
                   <v-btn round flat outline block color="indigo">Download</v-btn>
+                </v-flex>
+                <v-flex sm6 v-if="item.isLoading">
+                  <div class="half-circle-spinner" style="margin: auto">
+                    <div class="circle circle-1"></div>
+                    <div class="circle circle-2"></div>
+                  </div>
                 </v-flex>
               </v-layout>
             </v-card-actions>
@@ -78,21 +88,24 @@ export default {
         LastModified: "2019-03-24T15:04:33.000Z",
         ETag: '"730811aa2419582818146a4b5fce364f-5"',
         Size: 22786066,
-        StorageClass: "STANDARD"
+        StorageClass: "STANDARD",
+        isLoading: false
       },
       {
         Key: "StandaloneOSX.zip",
         LastModified: "2019-03-24T15:04:28.000Z",
         ETag: '"a33e48c9b75894b3fcbef76f8b1dd4d3-5"',
         Size: 24082815,
-        StorageClass: "STANDARD"
+        StorageClass: "STANDARD",
+        isLoading: false
       },
       {
         Key: "StandaloneWindows64.zip",
         LastModified: "2019-03-24T15:03:44.000Z",
         ETag: '"2f9936d88f7e8d7dc2022066fba520f9-6"',
         Size: 28105304,
-        StorageClass: "STANDARD"
+        StorageClass: "STANDARD",
+        isLoading: false
       }
     ]
   }),
@@ -101,7 +114,7 @@ export default {
     console.log(this.releases);
   },
   methods: {
-    listObjects: async () => {
+    listObjects: async function() {
       const { data } = await axios({
         method: "get",
         url: "/api/releases",
@@ -110,14 +123,66 @@ export default {
         }
       });
       console.log("====>", data);
+      this.isLoading = false;
     },
-    requestLink: async key => {
-      console.log("this is the requested key", key);
-      this.isLoading = true;
+    requestLink: function(item) {
+      console.log("this is the requested key", item);
+      //this.isLoading = true;
+      item.isLoading = true;
     }
   }
 };
 </script>
 
-<style>
+<style scoped>
+.spinner {
+  position: fixed;
+  z-index: 999;
+  overflow: visible;
+  margin: auto;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+
+.half-circle-spinner,
+.half-circle-spinner * {
+  box-sizing: border-box;
+}
+
+.half-circle-spinner {
+  width: 60px;
+  height: 60px;
+  border-radius: 100%;
+  position: relative;
+}
+
+.half-circle-spinner .circle {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border-radius: 100%;
+  border: calc(60px / 10) solid transparent;
+}
+
+.half-circle-spinner .circle.circle-1 {
+  border-top-color: #336699;
+  animation: half-circle-spinner-animation 1s infinite;
+}
+
+.half-circle-spinner .circle.circle-2 {
+  border-bottom-color: #336699;
+  animation: half-circle-spinner-animation 1s infinite alternate;
+}
+
+@keyframes half-circle-spinner-animation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 </style>
